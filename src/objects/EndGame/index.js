ee5a3e8463c12acad3faac4b740ffa5a6bad8b3b
RibamarRodrigues <ribamar326@gmail.com>
2023-05-16 20:33:59 -0300

    more changes in end screen and css

--- src/objects/EndGame/index.js
+++ src/objects/EndGame/index.js
@@ -1,35 +1,36 @@
-function VerifyEndGame(event) {
+import './style.css'
+
+function VerifyEndGame(gameMode) {
     const cards = document.querySelectorAll('.container')
     const score = document.querySelectorAll('.score')
     let points = []
+    //EndGame(gameMode)
     
-    if (event == "single") {
+    if (gameMode == "single") {
         if (cards.length/2 == score[0].textContent) 
-            EndGame()
+            EndGame(gameMode)
     } else {
         score.forEach(e => {
             points.push(Number(e.textContent))
         })
+
         points = points.reduce((a,b) => a+b)
-       
         if (cards.length/2 == points) 
-            EndGame()
+            EndGame(gameMode)
     }
 }
 
 
-function EndGame() {
-    location.reload()
-
+function EndGame(gameMode) {
     //test
     const $htmlEndSingle = `
     <div class="end-screen">
         <h1>Game Complete!!!</h1>
         <p>Time: 00:00</p>
         <p>Hit Rate: 0%</p>
-        <div>
-            <button>Home</button>
-            <button>Try Again</button>
+        <div class="end-screen-buttons">
+            <button class="home">Home</button>
+            <button class="try-again">Try Again</button>
         </div>
     </div>
     `
@@ -37,7 +38,7 @@ function EndGame() {
     <div class="end-screen">
         <h1>Player1 Wins!!!</h1>
         <p>Time: 00:00</p>
-        <p>
+        <p>Score: 0 vs 0</p>
         <div>
             <button>Home</button>
             <button>Try Again</button>
@@ -46,6 +47,15 @@ function EndGame() {
     `
     const $root = document.querySelector('#root')
     $root.insertAdjacentHTML("beforeend", $htmlEndSingle)
+
+    const endScreen = document.querySelector('.end-screen')
+    const items = endScreen.querySelectorAll('p')
+    const movements = Number(document.querySelector('.movements').textContent)
+    let score = document.querySelectorAll('.score')
+    score = Number(score[0].textContent)
+
+    const hitRate = Math.floor((score/movements)*100)
+    items[1].textContent = `Hit Rate: ${hitRate}%`
 }
 
 // Para fazer o calculo de porcentagem
